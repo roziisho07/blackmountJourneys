34dd11d7c26a8db03578a0a2a6e8c18c9071441d
roziisho07 <roziisho@gmail.com>
2024-12-07 02:56:02 +0500

    commit 2

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,6 +5,7 @@ export default async function Page() {
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="text-4xl font-bold">Home</h1>
       <hr />
+      <div>Hello sanity</div>
       <Link href="/posts">Posts index &rarr;</Link>
     </section>
   );
